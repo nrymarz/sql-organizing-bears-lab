23f00a3c809fd5b6208f059d83ec9865c6935294
nrymarz <nrymarz@gmail.com>
2020-11-15 23:33:42 -0600

    commit tree 2a7bf07848069be6701a5dd4c589b3533a245593

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr. Chocolate',5,'M','yellow','happy','dead');
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Rowdy',5,'F','yellow','happy','dead');
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Tabitha',5,'F','yellow','happy','dead');
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown',5,'M','yellow','happy','dead');
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Melissa',5,'F','yellow','happy','dead');
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Grinch',5,'M','yellow','happy','dead');
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Wendy',5,'F','yellow','happy','dead');
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,100000,'M','purple','angry','alive');
